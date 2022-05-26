48ea5102938b6f24a6c9da0a3c6bb051eb80653b
lancer <1271005467@qq.com>
2022-05-26 22:03:19 +0800

    init

---
+++ dist/bundle.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    // 可以考虑用接口来实现 接口可以和接口合并
+    String.prototype.double = function () {
+        return 'acbc';
+    };
+
+})();
+//# sourceMappingURL=bundle.js.map
